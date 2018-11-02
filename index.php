c5e9d1550289238300fd5909d57616b0f051402f
Wateen Afzal <m6.93wateen@gmail.com>
2018-11-02 19:09:31 +0500

    setup a switch to handle all incoming resquests

--- index.php
+++ index.php
@@ -2,3 +2,18 @@
 
 include "includes/connection.php";
 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$table_name = isset($_REQUEST['t']) ? $_REQUEST['t'] : '';
+$id = isset($_REQUEST['id']) ? $_REQUEST['id'] : 0;
+$request_method = strtolower($_SERVER['REQUEST_METHOD']);
+
+switch($request_method){
+    case "get":
+    case "post":
+    case "put":
+    case "delete":
+    default:
+        
+}
