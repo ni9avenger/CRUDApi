10b50d351906a0cbbe9f4f5cc5ea4a9b213812b9
Wateen Afzal <m6.93wateen@gmail.com>
2018-11-02 19:10:06 +0500

    implemented the get request response

--- index.php
+++ index.php
@@ -11,6 +11,19 @@ $request_method = strtolower($_SERVER['REQUEST_METHOD']);
 
 switch($request_method){
     case "get":
+        if($id != 0){
+            $sql = "SELECT * FROM $table_name WHERE id = '$id'";
+            $rs = mysqli_fetch_all(mysqli_query($conn,$sql),MYSQLI_ASSOC);
+            if(sizeof($rs) > 0)
+                echo json_encode($rs);
+            else
+                echo json_encode(array("message" => "No products found."));
+        }else{
+            $sql = "SELECT * FROM $table_name";
+            $rs = mysqli_fetch_all(mysqli_query($conn,$sql),MYSQLI_ASSOC);
+            echo json_encode($rs);
+        }
+        break;
     case "post":
     case "put":
     case "delete":
